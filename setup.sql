73d3f5629eabdd78808ab52e0eea0cad595a0900
joseph <jpshkt@umsystem.edu>
2024-04-24 17:22:38 -0500

    named each table in sql setupt

--- setup.sql
+++ setup.sql
@@ -1,1 +1,76 @@
 CREATE DATABASE recipeLibrary
+
+CREATE TABLE account (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+)
+
+CREATE TABLE admin (
+    
+)
+
+CREATE TABLE user (
+
+)
+
+CREATE TABLE cuisine (
+
+)
+
+CREATE TABLE regional_cuisine (
+
+)
+
+CREATE TABLE religious_cuisine (
+
+)
+
+CREATE TABLE recipe (
+
+)
+
+CREATE TABLE recipe_restrictions (
+
+)
+
+CREATE TABLE ingredients (
+
+)
+
+CREATE TABLE religious_restrictions (
+
+)
+
+CREATE TABLE user_restrictions (
+
+)
+
+CREATE TABLE cookbook (
+
+)
+
+CREATE TABLE favorite_recipes (
+
+)
+
+CREATE TABLE prefers (
+
+)
+
+CREATE TABLE contains (
+
+)
+
+CREATE TABLE deletes (
+
+)
+
+CREATE TABLE creates (
+
+)
+
+CREATE TABLE rates (
+
+)
