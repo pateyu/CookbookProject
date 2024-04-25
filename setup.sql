9cc3a4bd7975a4ba698b5eec4affcf7033832639
JosephSxhmo <167922404+JosephSxhmo@users.noreply.github.com>
2024-04-24 19:00:14 -0500

    Update setup.sql
    ysloslooki

--- setup.sql
+++ setup.sql
@@ -1,101 +1,80 @@
-CREATE DATABASE recipeLibrary
+--CREATE DATABASE recipeLibrary;
 
-CREATE TABLE account (
+CREATE TABLE IF NOT EXISTS account (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 
-CREATE TABLE admin (
+CREATE TABLE IF NOT EXISTS admin (
     Account_ID INT PRIMARY KEY,
     FOREIGN KEY(Account_ID) REFERENCES account(id)
 );
 
-CREATE TABLE user (    
+CREATE TABLE IF NOT EXISTS user (    
     Account_ID INT PRIMARY KEY,
     FOREIGN KEY(Account_ID) REFERENCES account(id)
 );
 
-CREATE TABLE cuisine (
+CREATE TABLE IF NOT EXISTS cuisine (
+    Cuisine_ID INT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS regional_cuisine (
+    religion_name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS religious_cuisine (
 
 );
 
-CREATE TABLE regional_cuisine (
+CREATE TABLE IF NOT EXISTS recipe (
 
 );
 
-CREATE TABLE religious_cuisine (
+CREATE TABLE IF NOT EXISTS recipe_restrictions (
 
 );
 
-CREATE TABLE recipe (
+CREATE TABLE IF NOT EXISTS ingredients (
 
 );
 
-CREATE TABLE recipe_restrictions (
+CREATE TABLE IF NOT EXISTS religious_restrictions (
 
 );
 
-CREATE TABLE ingredients (
-
-);
-
-CREATE TABLE religious_restrictions (
-
-);
-
-CREATE TABLE user_restrictions (
+CREATE TABLE IF NOT EXISTS user_restrictions (
     User_ID INT PRIMARY KEY,
-    UserRestriction VARCHAR(30) PRIMARY KEY,
+    UserRestriction VARCHAR(30),
     FOREIGN KEY(User_ID) REFERENCES account(id)
 );
 
-CREATE TABLE cookbook (
+CREATE TABLE IF NOT EXISTS cookbook (
 
 );
 
-CREATE TABLE favorite_recipes (
+CREATE TABLE IF NOT EXISTS favorite_recipes (
 
 );
 
-CREATE TABLE prefers (
+CREATE TABLE IF NOT EXISTS prefers (
 
 );
 
-CREATE TABLE contains (
-<<<<<<< HEAD
-    CookBook_ID INT FOREIGN KEY
-)
-
-CREATE TABLE deletes (
-    Admin_id INT FOREGIN KEY
-    recipe_name VARCHAR(80) FOREIGN KEY
-
-)
-
-CREATE TABLE creates (
-    User_ID INT FOREIGN KEY 
-    recipe_name VARCHAR(80) FOREIGN KEY
-);
-
-CREATE TABLE rates (
-    User_ID INTEGER FOREIGN KEY NOT NULL
-    recipe_name VARCHAR(80) FOREIGN KEY NOT NULL
-    user_rating INTEGER CHECK (BETWEEN 0 AND 5)
-=======
+CREATE TABLE IF NOT EXISTS contains (
 
 );
 
-CREATE TABLE deletes (
+CREATE TABLE IF NOT EXISTS deletes (
  
 );
 
-CREATE TABLE creates (
+CREATE TABLE IF NOT EXISTS creates (
 
 );
 
-CREATE TABLE rates (
+CREATE TABLE IF NOT EXISTS rates (
 
->>>>>>> f4499df3dda8531f38837ad600d11a6a5ad44611
 );
