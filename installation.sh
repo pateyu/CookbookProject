b8fb38fefad686257840236d53988f9edd4cf84b
Yug Patel <ybpkrk@mst.edu>
2024-09-22 00:30:37 -0500

    updated README

--- installation.sh
+++ installation.sh
@@ -3,17 +3,17 @@
 # Exit on any error
 set -e
 
-# Check if Python is installed
+# Check if Python 3 is installed
 if ! command -v python3 &> /dev/null
 then
    echo "Python3 is not installed. Installing Python3..."
    sudo apt update
-   sudo apt install python3 python3-pip -y
+   sudo apt install python3 -y
 else
    echo "Python3 is already installed."
 fi
 
-# Check if pip is installed
+# Check if pip3 is installed
 if ! command -v pip3 &> /dev/null
 then
    echo "pip3 is not installed. Installing pip3..."
@@ -22,18 +22,14 @@ else
    echo "pip3 is already installed."
 fi
 
-# Install Flask and other necessary packages
-echo "Installing Flask and other required packages..."
-pip3 install flask
-pip3 install flask_sqlalchemy
-
-
+# Install required Python packages from requirements.txt
+echo "Installing required Python packages from requirements.txt..."
+pip3 install -r requirements.txt
 
 # Initialize the database and start the application
 echo "Initializing the database and starting the application..."
 python3 app.py
 
-
-
 echo "Setup complete."
+
 
